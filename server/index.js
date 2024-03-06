677d03ddf4c2d235998ad2c01c5e9ea28e92d94e
Anthony Milton <110333974+ambivaly@users.noreply.github.com>
2024-03-06 13:48:10 -0800

    Changed port to align with server

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const fs = require('fs')
 const config = require('./config')
 
 const app = express();
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8000
 
 app.use(express.static(path.join(__dirname, 'portfolio')))
 app.use(express.static(path.join(__dirname, 'catan')))
