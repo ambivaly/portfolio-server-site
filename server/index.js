65d196d8026beb80529d2500081bbb1681a45d6f
Anthony Milton <110333974+ambivaly@users.noreply.github.com>
2024-03-08 13:31:17 -0800

    Changed from file to sockets
    Changed the server to use websockets instead of using json files, which makes a lot of the updates faster and saves space.

--- server/index.js
+++ server/index.js
@@ -3,13 +3,19 @@ const path = require('path');
 const AWS = require('aws-sdk')
 const fs = require('fs')
 const config = require('./config')
+const socketIo = require('socket.io');
+const http = require('http');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 8000
+const server = http.createServer(app);
+const io = socketIo(server);
 
 app.use(express.static(path.join(__dirname, 'portfolio')))
 app.use(express.static(path.join(__dirname, 'catan')))
 app.use(express.static(path.join(__dirname, 'adsb')))
+app.use(cors())
 
 
 AWS.config.update({
@@ -21,7 +27,37 @@ AWS.config.update({
 const dynamoDB = new AWS.DynamoDB();
 const tableName = config.tableName;
 
-const fetchDataAndWriteToFile = async () => {
+const sendDataToClients = async () => {
+    try {
+        const currentUnixTimestamp = Math.floor(new Date().getTime() / 1000);
+        const currentMin = Math.floor(currentUnixTimestamp / 60) * 60;
+
+        const params = {
+            TableName: tableName,
+            KeyConditionExpression: 'MinutePartition = :pk',
+            ExpressionAttributeValues: {
+                ':pk': { N: currentMin.toString() }
+            }
+        };
+
+        const result = await dynamoDB.query(params).promise();
+
+        if (result.Items.length > 0) {
+            const newData = result.Items;
+            const mostRecent = newData[newData.length-1];
+            io.emit('newData', mostRecent);
+            console.log('Data sent to clients successfully.');
+        } else {
+            console.log('Item not found in DynamoDB.');
+        }
+    } catch (error) {
+        console.error('Error sending data to clients:', error);
+    }
+};
+
+setInterval(sendDataToClients, 6000);
+
+/*const fetchDataAndWriteToFile = async () => {
     try {
 
         const currentUnixTimestamp = Math.floor(new Date().getTime() / 1000);
@@ -52,7 +88,7 @@ const fetchDataAndWriteToFile = async () => {
     }
 };
 
-setInterval(fetchDataAndWriteToFile, 6000);
+setInterval(fetchDataAndWriteToFile, 6000);*/
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'portfolio', 'index.html'))
@@ -66,6 +102,22 @@ app.get('/adsb', (req, res) => {
     res.sendFile(path.join(__dirname, 'adsb', 'index.html'))
 })
 
-app.listen(port, () => {
-    console.log('Server listening on port 8080')
+io.on('connection', (socket) => {
+    console.log('Client connected');
+
+    // Send initial data to the client
+    socket.emit('initialData', []);
+
+    // Cleanup on client disconnect
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
+/*app.listen(port, () => {
+    console.log('Server listening on port 8000')
+})*/
+
+server.listen(port, () => {
+    console.log('Websocket server listening on port 8000')
 })
