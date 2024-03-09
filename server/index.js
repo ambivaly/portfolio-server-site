f510909d55d6c58fa3a3f694dc8e8c6beff5bc1f
Anthony Milton <110333974+ambivaly@users.noreply.github.com>
2024-03-08 18:51:02 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,15 +11,15 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 8000;
 
-/*const server = http.createServer(app);*/
+const server = http.createServer(app);
 
-const server = https.createServer({
+/*const server = https.createServer({
     key: fs.readFileSync('./key.ssl'),
     cert: fs.readFileSync('./cert.ssl'),
     ca: fs.readFileSync('./ca.ssl'),
     requestCert: true,
     rejectUnauthorized: false
-}, app);
+}, app);*/
 
 const io = socketIo(server, {
     cors: {
